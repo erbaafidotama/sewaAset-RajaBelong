96fd511586a1192d7571373a826ec0d47fa1fd82
erb4f1dotama <erb4f1dotama@gmail.com>
2020-12-02 17:39:15 +0700

    setup login page

---
+++ src/components/Login.svelte
@@ -0,0 +1,89 @@
+<script>
+  import { loged } from "../stores/stores.js";
+
+  function login() {
+    loged.update("t");
+  }
+</script>
+
+<style>
+  #logreg-forms {
+    width: 412px;
+    margin: 10vh auto;
+    background-color: #f3f3f3;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  }
+  #logreg-forms .form-signin {
+    width: 100%;
+    max-width: 410px;
+    padding: 15px;
+    margin: auto;
+  }
+  #logreg-forms .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+  }
+  #logreg-forms .form-control:focus {
+    z-index: 2;
+  }
+  #logreg-forms .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  #logreg-forms .form-signin input[type="password"] {
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+
+  #logreg-forms button[type="submit"] {
+    margin-top: 10px;
+  }
+
+  .logo-login {
+    width: 180px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  /* Mobile */
+
+  @media screen and (max-width: 500px) {
+    #logreg-forms {
+      width: 300px;
+    }
+  }
+</style>
+
+<body>
+  <div id="logreg-forms">
+    <div class="form-signin">
+      <img src="img/rejang-lebong.png" class="logo-login" alt="" />
+      <h1 class="h3 mb-3 font-weight-normal" style="text-align: center">
+        Login
+      </h1>
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control"
+        placeholder="Email address"
+        required=""
+        autofocus="" />
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+        required="" />
+      <hr />
+      <button class="btn btn-success btn-block" on:click={login}><i
+          class="fas fa-sign-in-alt" />
+        Login</button>
+    </div>
+  </div>
+</body>
